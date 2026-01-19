6f090dc8cfc08b2866965ff3a6ce0f76f83de339
yuna-shimizu <masaki.ryu88@gmail.com>
2026-01-19 16:32:54 +0900

    create project

--- 1_agent.py
+++ 1_agent.py
@@ -2,7 +2,7 @@
 from strands import Agent
 
 # エージェントを作成
-agent = Agent("us.amazon.nova-2-lite-v1:0")
+agent = Agent("jp.anthropic.claude-sonnet-4-5-20250929-v1:0")
 
 # エージェントを起動
 agent("Strandsってどういう意味？")
